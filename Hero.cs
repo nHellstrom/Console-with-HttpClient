539eea23848f5cd49be8d08932cb9003ad188a8f
Niklas Hellström <102542068+nHellstrom@users.noreply.github.com>
2022-10-05 20:27:06 +0200

    Add project files.

---
+++ Hero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsoleHttpCaller
+{
+    internal class Hero
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+        [JsonPropertyName("birth_year")]
+        public string? Birthyear { get; set; }
+        [JsonPropertyName("gender")]
+        public string? Gender { get; set; }
+        [JsonPropertyName("height")]
+        public string? Height { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name,10}\nBirthyear: {Birthyear,10}\nGender: {Gender,10}\nHeight: {Height,10}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using static System.Console;
+using System.Text.Json;
+using System.Text;
+
+namespace ConsoleHttpCaller
+{
+    internal class Program
+    {
+        private static readonly HttpClient httpClient = new();
+
+        static async Task Main(string[] args)
+        {
+            await Handler();
+        }
+
+        private static async Task ProcessRepositories(string input)
+        {
+            try
+            {
+                //httpClient.DefaultRequestHeaders.Accept.Clear();
+                var stringTask = httpClient.GetStringAsync($"https://swapi.py4e.com/api/people/{input}");
+
+                var deserMsg = JsonSerializer.Deserialize<Hero>((string?)await stringTask);
+                WriteLine(deserMsg.ToString());
+            }
+            catch (HttpRequestException e)
+            {
+                WriteLine("404 cant find! Try a lower number!");
+            }
+        }
+
+        private static async Task Handler()
+        {
+            bool continuePick = true;
+            string? inpContinue = null;
+            int round = 1;
+
+            while(continuePick == true)
+            {
+                WriteLine($"Round {round++}!");
+                string input;
+
+                if (inpContinue is null)
+                {
+                    WriteLine("Pick a hero (number 1 - ?)!");
+                    input = ReadLine();
+                }
+                else
+                {
+                    input = inpContinue;
+                }
+                WriteLine(" - - Processing - -");
+
+                await ProcessRepositories(input);
+
+                WriteLine("\nWrite \"q\" to quit, or a number to continue!");
+                inpContinue = ReadLine();
+
+                if(inpContinue == "q")
+                {
+                    continuePick = false;
+                }
+                WriteLine("");
+            }
+        }
+    }
+}
